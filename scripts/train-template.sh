7084cdc6fd6d7b92ce9ef94fd8b26c68956a8b5e
andrew <inter>
2022-11-23 20:05:43 +0000

    added scripts to automate training experiments

--- scripts/train-template.sh
+++ scripts/train-template.sh
@@ -7,4 +7,5 @@ python ${ROOT_DIR}/train.py --data ${DATA_YAML} \
                 --project ${PROJ_DIR} \
                 --name ${EXP_NAME} \
                 --weights ${PROJ_DIR}/${EXP_NAME}/${INIT_WEIGHTS}  \
+                --exist-ok False \
                 --epochs ${EPOCHS}  2>&1 | tee ${PROJ_DIR}/${EXP_NAME}/info/out.log
